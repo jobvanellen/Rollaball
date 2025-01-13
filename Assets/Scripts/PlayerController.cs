b716f061c3ad858217c5ddb725d428bce166af55
Job van Ellen <job.vanellen@tmc.nl>
2025-01-13 14:15:32 +0100

    display score and winscreen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,11 +8,19 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 0;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public int amountOfPickups;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent <Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+
     }
 
     void OnMove(InputValue movementValue)
@@ -27,5 +36,24 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= amountOfPickups)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 }
